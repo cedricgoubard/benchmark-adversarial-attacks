6a3eae94c1ddb455651bca53d0791ae33e460b09
Cédric Goubard <cedric.goubard@wavestone.com>
2021-03-03 15:34:38 +0100

    Update requirements & setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='adv_benchmark',
-    version='0.0.1',
-    description='',
-    author='Timothée RIO',
-    packages=find_packages()
+    name="adv_benchmark",
+    version="0.1.0",
+    description="",
+    author="Timothée RIO, Cedric GOUBARD",
+    packages=find_packages(),
 )
